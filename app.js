6284e039396e01f3985ba64a76407aa8e616a8d7
Rahul Roy <devrahul2508@gmail.com>
2022-08-02 12:04:55 +0530

    Commits

--- app.js
+++ app.js
@@ -13,14 +13,53 @@ app.get("/",function(req,res){
 })
 
 app.post("/",function(req,res){
-    var fName = req.body.fname;
-    var lName = req.body.lname;
+    var firstName = req.body.fname;
+    var lastName = req.body.lname;
     var email = req.body.email;
 
-    console.log("FirstName"+fName+"LastName"+lName+"email"+email);
-})
+    var data ={
+        members :[
+            {
+                email_address : email,
+                status :  "subscribed",
+                merge_fields :{
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    var jsonData =JSON.stringify(data);
+
+    const url = "https://us17.api.mailchimp.com/3.0/lists/a2a44b70a5";
+    
+    const options={
+        method : "POST",
+        auth: "rahul:13ac3722def2af5c3a8b52c7ea27b767-us17"
+    }
+    
+    const request=https.request(url,options,function(response){
+
+        if(response.statusCode == 200){
+            res.sendFile(__dirname+"/success.html")
+        }
+        else{
+            res.sendFile(__dirname+"/failure.html")
+        }
+
+            response.on("data",function(data){
+                    console.log(JSON.parse(data));
+            })
+    })
+    request.write(jsonData);
+    request.end();
+});
 
 
 app.listen(3000,function(){
     console.log("Server started at port 3000");
 })
+
+//api key= 13ac3722def2af5c3a8b52c7ea27b767-us17
+//audience id = a2a44b70a5
